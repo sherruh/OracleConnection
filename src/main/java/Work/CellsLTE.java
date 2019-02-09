8ac5096e56e1f0afaebc66fd99d8dd6dae329774
sherruh <sherruh@gmail.com>
2019-02-09 21:09:48 +0600

    LTE Cells Commit

---
+++ src/main/java/Work/CellsLTE.java
@@ -0,0 +1,19 @@
+package Work;
+
+public class CellsLTE extends Cells {
+
+    private final String system="LTE";
+    private String pci;
+
+    public CellsLTE(String site, String latitude, String longitude, String cell,
+                    String channel, String cid, String direction, String beam,
+                    String height, String tilt,String pci) {
+        super(site, latitude, longitude, cell, channel, cid, direction, beam, height, tilt);
+        this.pci=pci;
+    }
+
+    @Override
+    public String toString() {
+        return system+"\t"+super.toString()+pci;
+    }
+}
